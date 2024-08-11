7154d3d409c7a51ea524c9e76498228d795fc4e5
vikas vobbilisetti <vikasvobbilisetti@vikass-Laptop.local>
2024-08-11 23:32:42 +0530

    3rd commmit

--- Test1.js
+++ Test1.js
@@ -1,7 +1,35 @@
 const express = require("express")
 const app = express()
-
+const {open} = require("sqlite")
+const sqlite3 = require("sqlite3")
+const path = require("path")
 app.get("/",(req,res)=>{
     res.send("Hello World");
 });
-app.listen(3000)
+
+
+let db = null
+const dbpath = path.join(__dirname,"travel.db")
+
+const iniDB = async ()=>{
+    try{
+    db = await open({
+        filename:dbpath,
+        driver:sqlite3.Database
+    });
+    app.listen(3000,()=>{
+        console.log("Server is running at port 3000.")
+    });
+
+    }
+    catch(e){
+        console.log(e);
+    }
+}
+iniDB()
+
+app.get("/viewall/",async (req,res)=>{
+    const getquery111 = "SELECT * FROM entries;";
+    const fulllist = await db.all(getquery111);
+    res.send(fulllist); 
+});
